9149729c41da6a456cbf58236c91cc2ad79ae7f3
Jiten Thakkar <jitenmt@gmail.com>
2014-04-26 18:56:48 +0530

    Google OAuth working

--- src/main/webapp/js/authenticateDb.js
+++ src/main/webapp/js/authenticateDb.js
@@ -1,64 +1,35 @@
-
-function authenticateDb(){
+function authenticateDb(callback){
 	console.log('here');
 		
 		var client = new Dropbox.Client({ key: "kxims84ljqwzxvp" });
-		return client.authenticate(function(error, client) {
-			  if (error) {
-			    
-			    return showError(error);
-			  }
-			  $("#unlink_db").click(function(){
-				 client.signOut({mustInvalidate: true}, function(error){
-					 if(error) {
-						 alert("Couldn't unlink");
-					 }
-					 $("#unlink_db").hide();
-					 alert('Unlinked');
-					 parent.location='index.html';
-				 });
-			  });
-			  $("#unlink_db").show();
-			  //console.log(client.credentials().token);
-			  //showFileList(client);
-		});
+		return client.authenticate(callback);
 };
-	function showError(error) {
-		  switch (error.status) {
-		  case Dropbox.ApiError.INVALID_TOKEN:
-		    authenticate();
-		    break;
+function showError(error) {
+	  switch (error.status) {
+	  case Dropbox.ApiError.INVALID_TOKEN:
+	    authenticate();
+	    break;
 
-		  case Dropbox.ApiError.NOT_FOUND:
-		    alert('File note found');
-		    break;
+	  case Dropbox.ApiError.NOT_FOUND:
+	    alert('File note found');
+	    break;
 
-		  case Dropbox.ApiError.OVER_QUOTA:
-		    alert('Dropbox quota is full');
-		    break;
+	  case Dropbox.ApiError.OVER_QUOTA:
+	    alert('Dropbox quota is full');
+	    break;
 
-		  case Dropbox.ApiError.RATE_LIMITED:
-		    alert('Too much traffic. Please try again later.');
-		    break;
+	  case Dropbox.ApiError.RATE_LIMITED:
+	    alert('Too much traffic. Please try again later.');
+	    break;
 
-		  case Dropbox.ApiError.NETWORK_ERROR:
-		    alert('Network problem, try again.');
-		    break;
+	  case Dropbox.ApiError.NETWORK_ERROR:
+	    alert('Network problem, try again.');
+	    break;
 
-		  case Dropbox.ApiError.INVALID_PARAM:
-		  case Dropbox.ApiError.OAUTH_ERROR:
-		  case Dropbox.ApiError.INVALID_METHOD:
-		  default:
-		    alert('Some technical error. Please refresh the page.');
-		  }
-		};
-	function showFileList(client) {
-		client.readdir("/", function(error, entries) {
-			  if (error) {
-			    return showError(error);  // Something went wrong.
-			  }
-
-			  //alert("Your Dropbox contains " + entries.join(". "));
-			  //var myApp = angular.module('myApp', ['angularTreeview']);
-			});
+	  case Dropbox.ApiError.INVALID_PARAM:
+	  case Dropbox.ApiError.OAUTH_ERROR:
+	  case Dropbox.ApiError.INVALID_METHOD:
+	  default:
+	    alert('Some technical error. Please refresh the page.');
+	  }
 	};
--- src/main/webapp/js/authenticateGd.js
+++ src/main/webapp/js/authenticateGd.js
@@ -1,20 +1,22 @@
-var CLIENT_ID = '<YOUR_CLIENT_ID>';
-      var SCOPES = 'https://www.googleapis.com/auth/drive';
+var CLIENT_ID = '811756332091-e7j905odjq9sbmsodlc2cksp4oqfb7qi.apps.googleusercontent.com';
+var SCOPES = 'https://www.googleapis.com/auth/drive';
 
   /**
    * Called when the client library is loaded to start the auth flow.
    */
-  function handleClientLoad() {
+  /*function handleClientLoad() {
     window.setTimeout(checkAuth, 1);
-  }
+  }*/
 
   /**
    * Check if the current user has authorized the application.
    */
-  function checkAuth() {
-    gapi.auth.authorize(
-        {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
-        handleAuthResult);
+  function authoriseGD(callback) {
+	  $(document).ready(function(){
+		gapi.auth.authorize(
+				{'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
+				callback);
+	  });
   }
 
   /**
@@ -23,21 +25,21 @@ var CLIENT_ID = '<YOUR_CLIENT_ID>';
    * @param {Object} authResult Authorization result.
    */
   function handleAuthResult(authResult) {
-    var authButton = document.getElementById('authorizeButton');
+    /*var authButton = document.getElementById('authorizeButton');
     var filePicker = document.getElementById('filePicker');
     authButton.style.display = 'none';
-    filePicker.style.display = 'none';
+    filePicker.style.display = 'none';*/
     if (authResult && !authResult.error) {
       // Access token has been successfully retrieved, requests can be sent to the API.
-      filePicker.style.display = 'block';
-      filePicker.onchange = uploadFile;
-    } else {
+      /*filePicker.style.display = 'block';
+      filePicker.onchange = uploadFile;*/
+      alert(authResult.access_token);
+    } else if(authResult.error) {
       // No access token could be retrieved, show the button to start the authorization flow.
-      authButton.style.display = 'block';
-      authButton.onclick = function() {
-          gapi.auth.authorize(
+      //authButton.style.display = 'block';
+      console.log(authResult.error.toString());
+      /*gapi.auth.authorize(
               {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
-              handleAuthResult);
-      };
-    }
+              handleAuthResult);*/
+      }
   }
